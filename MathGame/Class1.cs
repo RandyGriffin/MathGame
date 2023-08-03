f7f2034d1e63f572f89edadd8c85d0be6a1f38f8
RandyGriffin <randygriffin16@gmail.com>
2023-08-02 20:08:43 -0400

    Add project files.

---
+++ MathGame/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathGame
+{
+    internal class Class1
+    {
+        public Class1() //class where I write notes and stuff. Should be deleted lol
+        {
+            int index = 1;
+            string name = "Pablo";
+            char initial = 'P';
+            int year = 2022;
+            decimal height = 1.85m;
+            bool doWeLoveToCode = true;
+
+            // String interpolation $. use $@ to print multiple lines of code 
+            string myParagraph = $@"These are the most common data types:       
+        {index++} - string, example: {name}
+        {index++} - char, example: {initial}
+        {index++} - int, example: {year}
+        {index++} - decimal, example: {height}
+        {index++} - bool, example: {doWeLoveToCode}";
+
+            Console.WriteLine(myParagraph);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MathGame/Program.cs
@@ -0,0 +1,117 @@
+﻿using System.Threading.Tasks.Sources;
+
+var date = DateTime.Now;
+Console.WriteLine("Hello, What is your name?");
+var name = Console.ReadLine();
+Console.WriteLine("Hello {0}!, Today is {1} and welcome to a very simple math game!", name, date);
+Console.WriteLine(@$"Please choose an option from the menu!
+A -  Addition 
+S -  Subtraction 
+M -  Multiplication 
+D -  Division 
+Q -  To quit");
+
+
+var gameSelected = Console.ReadLine();
+
+switch (gameSelected.Trim().ToLower())
+{
+    case "a":
+        AddictionGame("Addiction game selected");
+        break;
+    case "s":
+        SubtractionGame("Subtraction game selected");
+        break;
+    case "m":
+        MultiplicationGame("Multiplication game selected");
+        break;
+    case "d":
+        DivisionGame("Division game selected");
+        break;
+    case "q":
+        Console.WriteLine("Thank you for playing!");
+        Environment.Exit(1);
+        break;
+    default:
+        Console.WriteLine("Invalid Input");
+        break;
+}
+
+//if (gameSelected == "a")
+//{
+//AddictionGame("Addiction game selected");
+
+//}
+//else if (gameSelected == "s")
+//{
+//SubtractionGame("Subtraction game selected");
+
+//}
+//else if (gameSelected == "m")
+//{
+//MultiplicationGame("Multiplication game selected");
+
+//}
+//else if (gameSelected == "d")
+//{
+//DivisionGame("Division game selected");
+
+//}
+//else if (gameSelected == "q")
+//{
+//    Console.WriteLine("Thank you for playing!");
+//    Environment.Exit(1);
+//}
+//else
+//{
+//    Console.WriteLine("Invalid input!");
+
+//}
+
+void DivisionGame(string message)
+{
+    Console.WriteLine(message);
+}
+
+void MultiplicationGame(string message)
+{
+    Console.WriteLine(message);
+}
+
+void SubtractionGame(string message)
+{
+    Console.WriteLine(message);
+}
+
+void AddictionGame(string message) //Methods
+{
+    Console.WriteLine(message);
+
+    var random = new Random();
+
+    var score = 0;
+    int firstNumber;
+    int secondNumber;
+
+    for (int i = 0; i < 5; i++)
+    {
+        firstNumber = random.Next(1, 9);
+        secondNumber = random.Next(1, 9);
+        Console.WriteLine($"{firstNumber} + {secondNumber}");
+        var result = Console.ReadLine();
+
+        if (int.Parse(result) == (firstNumber + secondNumber))
+        {
+            Console.WriteLine("Your answer was correct!");
+            score++;
+        }
+        else
+        {
+            Console.WriteLine("Your answer was incorrect");
+        }
+
+        if(i == 4)
+            Console.WriteLine($"Game over. Your final score is {score}");
+    }
+}
+
